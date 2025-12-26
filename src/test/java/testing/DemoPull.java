0eda18d647c525f7875e4455734f6c67c8bbda53
arjun <madhy2010@gmail.com>
2025-12-26 15:35:24 +0530

    intellij push

--- src/test/java/testing/DemoPull.java
+++ src/test/java/testing/DemoPull.java
@@ -3,6 +3,7 @@ package testing;
 public class DemoPull {
 	public static void main(String[] args) {
 		System.out.println("Hello Arjun");
+        System.out.println("hello venki from intellij");
 	}
 
 }
