1e8149bab7c5328e426c9516905d0f70e9e9bc00
Lenovo <Lenovo@DESKTOP-NRO38HH>
2025-12-31 12:18:04 +0530

    AAA

--- src/test/java/testing/Conflict.java
+++ src/test/java/testing/Conflict.java
@@ -3,8 +3,9 @@ package testing;
 import org.testng.annotations.Test;
 
 public class Conflict {
-@Test(groups = "smoke")
+	@Test(groups = "smoke")
 	public  void conflict() {
+		String BROWSER = System.getProperty("browser");
 		System.out.println("--conflict to venkatesh code--");
 		System.out.println("--conflict2 to venkatesh code--");
 		System.out.println("conflict3 ");
