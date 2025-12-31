d7984ac01b149ad31e5b128770224cee8f6f370a
arjun267472 <madhy2010@gmail.com>
2025-12-31 18:16:28 +0530

    Revert "conflict fork3"

--- src/test/java/testing/Conflict.java
+++ src/test/java/testing/Conflict.java
@@ -8,7 +8,6 @@ public class Conflict {
 		String BROWSER = System.getProperty("browser");
 		System.out.println("--conflict to venkatesh code--");
 		System.out.println("--conflict2 to venkatesh code--");
-		System.out.println("conflict3 ");
 
 	}
 }
